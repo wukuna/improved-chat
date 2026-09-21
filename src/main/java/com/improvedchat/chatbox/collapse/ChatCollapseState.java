460198fb6d517732b25785a32362fb5a5866acb4
wukuna <richbrown.94@gmail.com>
2026-09-21 18:58:54 -0500

    Add chat presentation features

---
+++ src/main/java/com/improvedchat/chatbox/collapse/ChatCollapseState.java
@@ -0,0 +1,4 @@
+/* Derived from Collapse Chat by stutify under BSD 2-Clause. See THIRD_PARTY_NOTICES.md. */
+package com.improvedchat.chatbox.collapse;
+
+public enum ChatCollapseState { UNKNOWN, COLLAPSED, EXPANDED }
