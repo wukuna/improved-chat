bc12802cc4964eda6d990520773a55d648805aa0
wukuna <richbrown.94@gmail.com>
2026-09-21 18:38:07 -0500

    Carry forward validated native chat fixes

--- src/main/java/com/improvedchat/chatbox/collapse/ChatCollapseModule.java
+++ src/main/java/com/improvedchat/chatbox/collapse/ChatCollapseModule.java
@@ -1,6 +1,4 @@
-/* Integrated and adapted from Collapse Chat by stutify under BSD 2-Clause.
- * See THIRD_PARTY_NOTICES.md. This module is hosted by ImprovedChatPlugin.
- */
+/* Improved Chat native chat collapse module. See THIRD_PARTY_NOTICES.md for required attribution. */
 package com.improvedchat.chatbox.collapse;
 
 import lombok.extern.slf4j.Slf4j;
@@ -75,12 +73,6 @@ public class ChatCollapseModule {
 
     private void refreshChatWidgets() {
         widgetManager.updateChatWidgets(state);
-    }
-
-    public void refreshAfterExternalStyleChange() {
-        if (started) {
-            clientThread.invokeLater(this::refreshChatWidgets);
-        }
     }
 
     @Subscribe
